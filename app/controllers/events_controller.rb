3a6309c076d35c1d7b4f612dc69ff331cf55ffb4
Vermuth <dmsunbox@gmail.com>
2014-03-20 00:03:49 +0400

    add method [index...destroy]

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,51 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      flash[:notice] = "Event created"
+      redirect_to @event
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update_attributes(event_params)
+      flash[:notice] = "Update event"
+      redirect_to @event
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    flash[:notice] = "Destoroyed event"
+    redirect_to events_index
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :event_time)
+  end
+
 end
