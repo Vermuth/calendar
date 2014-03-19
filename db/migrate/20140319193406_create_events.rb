8899f1f1bd738d706f2500abb6b68d335e2b2d4a
Vermuth <dmsunbox@gmail.com>
2014-03-19 23:37:03 +0400

    fix mistake in word published_on

--- db/migrate/20140319193406_create_events.rb
+++ db/migrate/20140319193406_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.string :name
-      t.date :published_od
+      t.date :published_on
       t.integer :user_id
 
       t.timestamps
