790ec92cc4c8ebc5de7c1fba8ef61e6a83429fbf
Vermuth <dmsunbox@gmail.com>
2014-03-28 15:38:43 +0400

    add validation for every: day. week. month, year

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,5 +4,9 @@ class Event < ActiveRecord::Base
   validates :name,        :presence => true
   validates :event_time,  :presence => true
   validates :see_all,     :presence => true
+  validates :every_day,   :presence => true
+  validates :every_week,  :presence => true
+  validates :every_month, :presence => true
+  validates :every_year,  :presence => true
 
 end
